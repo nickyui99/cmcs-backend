03a93cfc0119af60088c969616842db073b59710
nickyui99 <93330502+nickyui99@users.noreply.github.com>
2022-12-16 23:15:32 +0800

    fix array arrangement issues

--- config.js
+++ config.js
@@ -1,27 +1,27 @@
 const {Sequelize} = require("sequelize");
 
 
-const sequelize = new Sequelize("cmcs", "root", "", {
-	host: "127.0.0.1",
-	dialect: "mysql",
-	define: {
-		timestamps: false,
-	}
-});
-
-// const sequelize = new Sequelize("sql6582248", "sql6582248", "l6qg7ePrQU", {
-// 	host: "sql6.freesqldatabase.com",
+// const sequelize = new Sequelize("cmcs", "root", "", {
+// 	host: "127.0.0.1",
 // 	dialect: "mysql",
 // 	define: {
 // 		timestamps: false,
-// 	},
-// 	pool: {
-// 		max: 5,
-// 		min: 0,
-// 		acquire: 30000,
-// 		idle: 10000
 // 	}
 // });
+
+const sequelize = new Sequelize("sql6582248", "sql6582248", "l6qg7ePrQU", {
+	host: "sql6.freesqldatabase.com",
+	dialect: "mysql",
+	define: {
+		timestamps: false,
+	},
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
+	}
+});
 
 // const sequelize = new Sequelize("cmcs", "nick@cmcs1", "Nicholas12345!", {
 // 	host: "cmcs1.database.windows.net",
