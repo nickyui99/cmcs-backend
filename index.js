38676b1e7835d4825ee3812442da3f90cd1733be
nickyui99 <93330502+nickyui99@users.noreply.github.com>
2023-03-02 17:20:25 +0800

    - added google signin features - optimize code performance

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ sequelize.authenticate().then(() => {
 
 sequelize.sync().then(r =>{});
 
-const server = app.listen(3000, function () {});
+// const server = app.listen(3000, function () {});
 
-// const server = app.listen(process.env.PORT || 5000, () => {
-// 	const port = server.address().port;
-// 	console.log(`Express is working on port ${port}`);
-// });
+const server = app.listen(process.env.PORT || 5000, () => {
+	const port = server.address().port;
+	console.log(`Express is working on port ${port}`);
+});
